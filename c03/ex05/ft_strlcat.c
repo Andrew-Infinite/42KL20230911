4b2411a91b737e142a998cfe73b567c940d685dc
Andrew Jian An Tay <atay@u82z05s03.42kl.edu.my>
2023-09-21 08:29:53 +0800

    header file all added

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -1,14 +1,16 @@
-/******************************************************************************
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atay <atay@42kl.edu.my>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/21 08:24:17 by atay              #+#    #+#             */
+/*   Updated: 2023/09/21 08:24:19 by atay             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-#include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 
 unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
 {
@@ -30,13 +32,3 @@ unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
 
     return i+1;
 }
-int main()
-{
-    char str[7] = "123";
-    char str2[] = "sdf";
-    printf("%d \n",ft_strlcat(str,str2,6));
-    printf("%s",str);
-
-    return 0;
-}
-
