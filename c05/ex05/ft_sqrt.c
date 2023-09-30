b3637291710907f6d709cfba7448ba512f62f92c
Andrew <b07611024@gmail.com>
2023-09-30 10:13:32 +0800

    fix norm c5

--- c05/ex05/ft_sqrt.c
+++ c05/ex05/ft_sqrt.c
@@ -10,42 +10,48 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int binary_guess(int nb, int upper, int lower)
+int	binary_guess(int nb, int upper, int lower)
 {
-	int mid;
-	
-	mid = (int)((upper - lower)/2 + lower);
-	if(upper > lower + 1)
+	int	mid;
+
+	mid = (int)((upper - lower) / 2 + lower);
+	if (upper > lower + 1)
 	{
-		if( nb%mid == 0 && nb/mid == mid)
-			return mid;
-		else if((nb/mid) < mid)
-			mid = binary_guess(nb,mid,lower);
-		else if((nb/mid) >= mid)
-			mid = binary_guess(nb,upper,mid);
+		if (nb % mid == 0 && nb / mid == mid)
+			return (mid);
+		else if ((nb / mid) < mid)
+			mid = binary_guess(nb, mid, lower);
+		else if ((nb / mid) >= mid)
+			mid = binary_guess(nb, upper, mid);
 	}
 	else
-		return 0;
-	return mid;
-}
-int	ft_sqrt(int nb)
-{
-	if(nb<4 && nb!= 1)
-		return 0;
-	if(nb==1)
-		return 1;
-	return binary_guess(nb,nb,0);
+		return (0);
+	return (mid);
 }
 
-#include <stdio.h>
-int main (){
+int	ft_sqrt(int nb)
+{
+	if (nb < 4 && nb != 1)
+		return (0);
+	if (nb == 1)
+		return (1);
+	return (binary_guess(nb, nb, 0));
+}
+
+/*#include <stdio.h>
+
+int	main(void)
+{
 	int j = 1;
-	for(int i = 0; i < 2147483647; i++){
+	for (int i = 0; i < 2147483647; i++)
+	{
 		int temp = ft_sqrt(i);
-		if(temp>0){
-			if((i%temp != 0 || i/temp != temp)){
-				printf("%d %d %d\n",temp,i,j);
+		if (temp > 0)
+		{
+			if ((i % temp != 0 || i / temp != temp))
+			{
+				printf("%d %d %d\n", temp, i, j);
 			}
 		}
 	}
-}
+}*/
--- c05/ex06/ft_is_prime.c
+++ c05/ex06/ft_is_prime.c
@@ -10,36 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int binary_guess(int nb, int upper, int lower)
+int	binary_guess(int nb, int upper, int lower)
 {
-	int mid;
-	
-	mid = (int)((upper - lower)/2 + lower);
-	if(upper > lower + 1)
+	int	mid;
+
+	mid = (int)((upper - lower) / 2 + lower);
+	if (upper > lower + 1)
 	{
-		if( nb%mid == 0 && nb/mid == mid)
-			return mid;
-		else if((nb/mid) < mid)
-			mid = binary_guess(nb,mid,lower);
-		else if((nb/mid) >= mid)
-			mid = binary_guess(nb,upper,mid);
+		if (nb % mid == 0 && nb / mid == mid)
+			return (mid);
+		else if ((nb / mid) < mid)
+			mid = binary_guess(nb, mid, lower);
+		else if ((nb / mid) >= mid)
+			mid = binary_guess(nb, upper, mid);
 	}
 	else
-		return upper;
-	return mid;
+		return (upper);
+	return (mid);
 }
 
 int	ft_sqrt(int nb)
 {
-	if(nb<10)
-		return nb/2;
-	return binary_guess(nb,nb,0);
+	if (nb < 10)
+		return (nb / 2);
+	return (binary_guess(nb, nb, 0));
 }
 
 int	ft_is_prime(int nb)
 {
 	int	i;
-	int upper;
+	int	upper;
 
 	upper = ft_sqrt(nb);
 	if (nb < 2)
@@ -54,10 +54,12 @@ int	ft_is_prime(int nb)
 	return (1);
 }
 
+/*
 #include <stdio.h>
+
 int main (){
 	for(int i = 0; i < 200;i++){
 		if (ft_is_prime(i)>0)
 			printf("%d, ",i);
 	}
-}
+}*/
